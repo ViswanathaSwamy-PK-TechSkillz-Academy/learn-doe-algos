4a2ed9c0d6b3ad5d5e98522501f2a8c60add7862
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-14 12:36:03 +0530

    Swamy/14may work (#2)
    * Update main.js
    
    * Changes
    
    * Changes

---
+++ src/TwoPointerPalindrome/CS/Tpp.Algo/Program.cs
@@ -0,0 +1,35 @@
+﻿string[] testCases = { "RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR" };
+int i = 1;
+
+foreach (string s in testCases)
+{
+    WriteLine($"Test Case #{i}");
+    WriteLine(new string('-', 100));
+    WriteLine($"The input string is '{s}' and the length of the string is {s.Length}.");
+    WriteLine("\nIs it a palindrome?..... " + IsPalindrome(s));
+    WriteLine(new string('-', 100));
+    i++;
+}
+
+static bool IsPalindrome(string s)
+{
+    int left = 0;
+    int right = s.Length - 1;
+
+    while (left < right)
+    {
+        WriteLine($"Current elements left index: '{s[left]}' and right index: '{s[right]}'");
+
+        if (s[left] != s[right])
+        {
+            WriteLine("The elements aren't the same, hence we return false");
+            return false;
+        }
+
+        left++; // Heading towards the right
+        right--; // Heading towards the left
+    }
+
+    // We reached the middle of the string without finding a mismatch, so it is a palindrome.
+    return true;
+}
