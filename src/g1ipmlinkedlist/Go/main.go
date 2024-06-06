3929a855ee58ae13ccb1e036f270e9cce00d9835
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-06 13:22:54 +0530

    Update main.go

--- src/g1ipmlinkedlist/Go/main.go
+++ src/g1ipmlinkedlist/Go/main.go
@@ -2,31 +2,42 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func reverse(head *LinkedListNode) *LinkedListNode {
 	var prev, next *LinkedListNode
 	current := head
+
 	for current != nil {
 		next = current.next
 		current.next = prev
 		prev = current
 		current = next
 	}
+
 	return prev
 }
 
 func main() {
-	// Create a linked list
-	list := LinkedList{}
-	list.CreateLinkedList([]int{1, 2, 3, 4, 5})
+	input := [][]int{
+		{1, 2, 3, 4, 5},
+		{1, 2, 3, 4, 5, 6},
+		{3, 2, 1},
+		{10},
+		{1, 2},
+	}
 
-	fmt.Println("Original Linked List:")
-	list.DisplayLinkedList()
+	for i := 0; i < len(input); i++ {
+		inputLinkedList := LinkedList{}
 
-	// Reverse the linked list
-	list.head = reverse(list.head)
+		inputLinkedList.CreateLinkedList(input[i])
+		fmt.Printf("%d. \tInput linked list: ", i+1)
 
-	fmt.Println("\nReversed Linked List:")
-	list.DisplayLinkedList()
+		DisplayLinkedListWithForwardArrow(inputLinkedList.head)
+		fmt.Printf("\n\tReversed linked list: ")
+
+		DisplayLinkedListWithForwardArrow(reverse(inputLinkedList.head))
+		fmt.Printf("\n%s\n", strings.Repeat("-", 100))
+	}
 }
