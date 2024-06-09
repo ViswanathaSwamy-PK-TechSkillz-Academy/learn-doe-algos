4182f11f04e70fd9f7f2efad490c3b1c23d6b557
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-09 13:44:32 +0530

    Moved the usage logic into another file

--- src/A2SumOfThreeValues/JS/main.js
+++ src/A2SumOfThreeValues/JS/main.js
@@ -1,27 +1,4 @@
-function findSumOfThree(nums, target) {
-    nums.sort((a, b) => a - b);  // Sort the array in ascending order
-
-    for (let i = 0; i < nums.length - 2; i++) {
-        let left = i + 1;
-        let right = nums.length - 1;
-
-        while (left < right) {
-            const sum = nums[i] + nums[left] + nums[right];
-
-            if (sum === target) {
-                return true;  // Triplet found!
-            } else if (sum < target) {
-                left++;  // Move left pointer to increase the sum
-            } else {
-                right--;  // Move right pointer to decrease the sum
-            }
-        }
-    }
-
-    return false;  // Triplet not found
-}
-
-module.exports = { findSumOfThree };
+const { findSumOfThree } = require('./findSumOfThree');
 
 // Example usage
 function main() {
