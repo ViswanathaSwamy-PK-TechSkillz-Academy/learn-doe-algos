b8950e18baae939e99c6395d82b78568253b9ce9
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-04 16:20:45 +0530

    Swamy/03jul work (#18)
    * Changes
    
    * Changes

---
+++ src/a1twopointerpalindrome/CS/Tpp.Algo/Palindrome.cs
@@ -0,0 +1,29 @@
+// Palindrome.cs
+using System;
+
+namespace Tpp.Algo;
+
+public static class PalindromeChecker
+{
+    public static bool IsPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.Length - 1;
+
+        while (left < right)
+        {
+            Console.WriteLine($"Current elements left index: '{s[left]}' and right index: '{s[right]}'");
+
+            if (s[left] != s[right])
+            {
+                Console.WriteLine("The elements aren't the same, hence we return false");
+                return false;
+            }
+
+            left++; // Heading towards the right
+            right--; // Heading towards the left
+        }
+
+        return true;
+    }
+}
--- src/a1twopointerpalindrome/Go/main.go
+++ src/a1twopointerpalindrome/Go/main.go
@@ -5,23 +5,6 @@ import (
 	"strings"
 )
 
-func isPalindrome(s string) bool {
-	left := 0
-	right := len(s) - 1
-
-	for left < right {
-		if s[left] != s[right] {
-			fmt.Println("The elements aren't the same, hence we return false")
-			return false
-		}
-
-		left++  // Heading towards the right
-		right-- // Heading towards the left
-	}
-
-	return true // We reached the middle of the string without finding a mismatch, so it is a palindrome.
-}
-
 func main() {
 	testCases := []string{"RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"}
 	i := 1
