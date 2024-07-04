b0560901d39f9a8e21e2072c7f4839b9354ef604
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-04 16:20:10 +0530

    Changes

--- src/a1twopointerpalindrome/CS/Tpp.Algo/Program.cs
+++ src/a1twopointerpalindrome/CS/Tpp.Algo/Program.cs
@@ -1,26 +1,6 @@
-﻿static bool IsPalindrome(string s)
-{
-    int left = 0;
-    int right = s.Length - 1;
+﻿using Tpp.Algo;
 
-    while (left < right)
-    {
-        WriteLine($"Current elements left index: '{s[left]}' and right index: '{s[right]}'");
-
-        if (s[left] != s[right])
-        {
-            WriteLine("The elements aren't the same, hence we return false");
-            return false;
-        }
-
-        left++; // Heading towards the right
-        right--; // Heading towards the left
-    }
-
-    return true;
-}
-
-string[] testCases = ["RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"];
+string[] testCases = { "RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR" };
 int i = 1;
 
 foreach (string s in testCases)
@@ -28,8 +8,7 @@ foreach (string s in testCases)
     WriteLine($"Test Case #{i}");
     WriteLine(new string('-', 100));
     WriteLine($"The input string is '{s}' and the length of the string is {s.Length}.");
-    WriteLine("\nIs it a palindrome?..... " + IsPalindrome(s));
+    WriteLine("\nIs it a palindrome?..... " + PalindromeChecker.IsPalindrome(s));
     WriteLine(new string('-', 100));
     i++;
 }
-
