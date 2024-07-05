34929f5e60f58a527a13a4c5f18b3d87705bd15c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-05 09:04:38 +0530

    Swamy/04jul work (#21)
    * Changes
    
    * Update .gitignore
    
    * Changes

--- src/a1twopointerpalindrome/JS/main.js
+++ src/a1twopointerpalindrome/JS/main.js
@@ -1,24 +1,5 @@
-function isPalindrome(s) {
-    let left = 0,
-        right = s.length - 1;
-
-    while (left < right) {
-        console.log(`Current elements left index: '${s[left]}' and right index: '${s[right]}'`);
-
-        if (s[left] != s[right]) { // If the elements at index l and index r are not equal,
-            console.log("The elements aren't the same, hence we return false");
-            return false;
-        }
-
-        left++;     // Heading towards the right
-        right--;    // Heading towards the left
-    }
-
-    
-    return true;    // We reached the middle of the string without finding a mismatch, so it is a palindrome.
-}
-
-module.exports = { isPalindrome };
+// main.js
+const { isPalindrome } = require('./palindromeChecker');
 
 function main() {
     let testCases = ["RACEACAR", "A", "ABCDEFGFEDCBA", "ABC", "ABCBA", "ABBA", "RACEACAR"], i = 1;
