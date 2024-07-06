f2e38a009f10130e33608acb828dd486f29cacb0
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-06 12:31:54 +0530

    Chnages

---
+++ src/b1happynumber/CS/FSP.Algo/HappyNumberHelper.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+
+namespace FSP.Algo;
+
+public class HappyNumberHelper
+{
+    // Helper function that calculates the sum of squared digits
+    private static int SumOfSquaredDigits(int number)
+    {
+        int totalSum = 0;
+        while (number > 0)
+        {
+            int digit = number % 10;
+            number /= 10;
+            totalSum += digit * digit;
+        }
+        return totalSum;
+    }
+
+    // Function to check if a number is a happy number
+    public static bool IsHappyNumber(int n)
+    {
+        HashSet<int> seen = new HashSet<int>();
+
+        while (n != 1 && !seen.Contains(n))
+        {
+            seen.Add(n);
+            n = SumOfSquaredDigits(n);
+        }
+
+        return n == 1;
+    }
+}
---
+++ src/b1happynumber/CS/FSP.Algo/Program.cs
@@ -0,0 +1,10 @@
+﻿using FSP.Algo;
+
+int[] inputs = { 1, 5, 19, 25, 7 };
+
+foreach (int input in inputs)
+{
+    WriteLine($"Input Number: {input}");
+    WriteLine($"Is it a happy number? {HappyNumberHelper.IsHappyNumber(input)}");
+    WriteLine(new string('-', 50));
+}
