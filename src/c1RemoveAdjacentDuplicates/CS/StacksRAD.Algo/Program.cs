99c9efd34f71a2889d110dcd073ca2f82f80e608
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-08 16:46:18 +0530

    Changes

---
+++ src/c1RemoveAdjacentDuplicates/CS/StacksRAD.Algo/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace StacksRAD.Algo;
+using System.Collections.Generic;
+
+var inputs = new List<string>
+        {
+            "g",
+            "ggaabcdeb",
+            "abbddaccaaabcd",
+            "aannkwwwkkkwna",
+            "abbabccblkklu"
+        };
+
+for (int i = 0; i<inputs.Count; i++)
+{
+    string input = inputs[i];
+    WriteLine($"{i + 1}.\tRemove duplicates from string: \"{input}\"");
+    string resultingString = RemoveDuplicatesHelper.RemoveDuplicates(input);
+    WriteLine($"\tString after removing duplicates: \"{resultingString}\"");
+    WriteLine(new string ('-', 100));
+}
