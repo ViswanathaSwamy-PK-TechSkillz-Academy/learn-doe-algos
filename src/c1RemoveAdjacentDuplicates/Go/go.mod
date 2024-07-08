05b30125bb47b2a0650bfe01065885c5a402f6e5
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-08 16:26:09 +0530

    Changes

--- src/c1RemoveAdjacentDuplicates/Go/go.mod
+++ src/c1RemoveAdjacentDuplicates/Go/go.mod
@@ -1,3 +1,3 @@
 module A5RemoveAdjacentDuplicates
 
-go 1.22.3
+go 1.22.5
--- src/c1RemoveAdjacentDuplicates/Go/main.go
+++ src/c1RemoveAdjacentDuplicates/Go/main.go
@@ -5,22 +5,6 @@ import (
 	"strings"
 )
 
-func removeDuplicates(s string) string {
-
-	stack := []rune{}
-
-	for _, char := range s {
-		if len(stack) > 0 && stack[len(stack)-1] == char {
-			stack = stack[:len(stack)-1] // pop the last element
-		} else {
-			stack = append(stack, char) // push the current character
-		}
-		fmt.Printf("\tStack: %s\n", string(stack))
-	}
-
-	return string(stack)
-}
-
 func main() {
 	inputs := []string{
 		"g",
