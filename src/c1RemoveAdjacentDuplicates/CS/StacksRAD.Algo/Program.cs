b2b0ca8e1ed637553d7dada0d45d44e704ea0b84
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-08 16:47:43 +0530

    Update Program.cs

--- src/c1RemoveAdjacentDuplicates/CS/StacksRAD.Algo/Program.cs
+++ src/c1RemoveAdjacentDuplicates/CS/StacksRAD.Algo/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace StacksRAD.Algo;
+﻿using StacksRAD.Algo;
 using System.Collections.Generic;
 
 var inputs = new List<string>
@@ -10,11 +10,11 @@ var inputs = new List<string>
             "abbabccblkklu"
         };
 
-for (int i = 0; i<inputs.Count; i++)
+for (int i = 0; i < inputs.Count; i++)
 {
     string input = inputs[i];
     WriteLine($"{i + 1}.\tRemove duplicates from string: \"{input}\"");
     string resultingString = RemoveDuplicatesHelper.RemoveDuplicates(input);
     WriteLine($"\tString after removing duplicates: \"{resultingString}\"");
-    WriteLine(new string ('-', 100));
+    WriteLine(new string('-', 100));
 }
