e6eb24c17e475d93dfb688f93fee394fc51cd39d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-15 17:40:35 +0530

    Update hash_map.go

--- src/d1hashmaps/Go/hash_map.go
+++ src/d1hashmaps/Go/hash_map.go
@@ -1,16 +1,17 @@
 package main
 
-const KeySpace = 2069 // Define the size of the key space for hashing
-
 // DesignHashMap represents a hash map.
 type DesignHashMap struct {
-	buckets []*Bucket // Slice of pointers to Buckets
+	keySpace int
+	buckets  []*Bucket // Slice of pointers to Buckets
 }
 
 // Constructor initializes the hash map.
 func Constructor() DesignHashMap {
+	const keySpace = 2069
 	hashMap := DesignHashMap{
-		buckets: make([]*Bucket, KeySpace),
+		keySpace: keySpace,
+		buckets:  make([]*Bucket, keySpace),
 	}
 	for i := range hashMap.buckets {
 		hashMap.buckets[i] = NewBucket()
@@ -20,18 +21,18 @@ func Constructor() DesignHashMap {
 
 // Put inserts a key-value pair into the hash map or updates the value if the key already exists.
 func (hm *DesignHashMap) Put(key int, value int) {
-	hashKey := key % KeySpace
+	hashKey := key % hm.keySpace
 	hm.buckets[hashKey].Update(key, value)
 }
 
 // Get returns the value associated with the key if it exists, otherwise returns -1.
 func (hm *DesignHashMap) Get(key int) int {
-	hashKey := key % KeySpace
+	hashKey := key % hm.keySpace
 	return hm.buckets[hashKey].Get(key)
 }
 
 // Remove deletes the key-value pair from the hash map if it exists.
 func (hm *DesignHashMap) Remove(key int) {
-	hashKey := key % KeySpace
+	hashKey := key % hm.keySpace
 	hm.buckets[hashKey].Remove(key)
 }
