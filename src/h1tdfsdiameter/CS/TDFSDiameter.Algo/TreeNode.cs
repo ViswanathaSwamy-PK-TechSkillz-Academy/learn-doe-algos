c471c42a252bbf111b77e833674d233e31f93d05
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-27 14:26:51 +0530

    Code refactoring

--- src/h1tdfsdiameter/CS/TDFSDiameter.Algo/TreeNode.cs
+++ src/h1tdfsdiameter/CS/TDFSDiameter.Algo/TreeNode.cs
@@ -3,8 +3,8 @@
 public class TreeNode<T>
 {
     public T Data { get; set; }
-    public TreeNode<T> Left { get; set; }
-    public TreeNode<T> Right { get; set; }
+    public TreeNode<T>? Left { get; set; }
+    public TreeNode<T>? Right { get; set; }
 
     public TreeNode(T data)
     {
