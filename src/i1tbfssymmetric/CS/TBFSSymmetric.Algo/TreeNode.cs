4efbc055dccbb98759e2ea5dbdbfe8117d686fa6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-07-31 13:11:26 +0530

    TreeNode, and BinaryTree

---
+++ src/i1tbfssymmetric/CS/TBFSSymmetric.Algo/TreeNode.cs
@@ -0,0 +1,15 @@
+namespace TBFSSymmetric.Algo;
+
+public class TreeNode<T>
+{
+    public T Data { get; set; }
+    public TreeNode<T>? Left { get; set; }
+    public TreeNode<T>? Right { get; set; }
+
+    public TreeNode(T data)
+    {
+        Data = data;
+        Left = null;
+        Right = null;
+    }
+}
