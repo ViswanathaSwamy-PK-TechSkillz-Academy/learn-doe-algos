e37b3be10278b03a95c692fa8d9a036efe31d63b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-02 16:24:34 +0530

    Code Refactoring

--- src/j1bwmfinddifference/CS/BWMFindDifference.Algo/FindDifference.cs
+++ src/j1bwmfinddifference/CS/BWMFindDifference.Algo/FindDifference.cs
@@ -7,26 +7,17 @@ public static class DifferenceFinder
     public static int ExtraCharacterIndex(string str1, string str2)
     {
         int result = 0;
-        int str1Length = str1.Length;
-        int str2Length = str2.Length;
 
-        for (int i = 0; i < str1Length; i++)
+        for (int i = 0; i < str1.Length; i++)
         {
             result ^= str1[i];
         }
 
-        for (int i = 0; i < str2Length; i++)
+        for (int i = 0; i < str2.Length; i++)
         {
             result ^= str2[i];
         }
 
-        if (str1Length > str2Length)
-        {
-            return str1.IndexOf((char)result);
-        }
-        else
-        {
-            return str2.IndexOf((char)result);
-        }
+        return str1.Length > str2.Length ? str1.IndexOf((char)result) : str2.IndexOf((char)result);
     }
 }
--- src/j1bwmfinddifference/CS/BWMFindDifference.Algo/Program.cs
+++ src/j1bwmfinddifference/CS/BWMFindDifference.Algo/Program.cs
@@ -2,13 +2,13 @@
 
 using BWMFindDifference.Algo;
 
-string[] string1List = { "wxyz", "cbda", "jlkmn", "courae", "hello" };
-string[] string2List = { "zwxgy", "abc", "klmn", "couearg", "helo" };
+string[] string1List = ["wxyz", "cbda", "jlkmn", "courae", "hello"];
+string[] string2List = ["zwxgy", "abc", "klmn", "couearg", "helo"];
 
 for (int i = 0; i < string1List.Length; i++)
 {
-    Console.WriteLine($"{i + 1}.\tString 1 = {string1List[i]}");
-    Console.WriteLine($"\tString 2 = {string2List[i]}");
-    Console.WriteLine($"\n\tThe extra character is at index: {DifferenceFinder.ExtraCharacterIndex(string1List[i], string2List[i])}");
-    Console.WriteLine(new string('-', 100));
+    WriteLine($"{i + 1}.\tString 1 = {string1List[i]}");
+    WriteLine($"\tString 2 = {string2List[i]}");
+    WriteLine($"\n\tThe extra character is at index: {DifferenceFinder.ExtraCharacterIndex(string1List[i], string2List[i])}");
+    WriteLine(new string('-', 100));
 }
