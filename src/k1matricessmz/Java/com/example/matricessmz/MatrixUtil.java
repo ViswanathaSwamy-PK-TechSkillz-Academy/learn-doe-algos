0855608e35cf3ad14e11305b9531aadb642fb3d7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-08-04 11:21:40 +0530

    Algo in Java

---
+++ src/k1matricessmz/Java/com/example/matricessmz/MatrixUtil.java
@@ -0,0 +1,14 @@
+// File: MatrixUtil.java
+
+package com.example.matricessmz;
+
+public class MatrixUtil {
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+}
