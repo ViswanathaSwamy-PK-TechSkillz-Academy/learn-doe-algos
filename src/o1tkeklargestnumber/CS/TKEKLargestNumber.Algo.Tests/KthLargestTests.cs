75c744fcb166463de222c11cfd5500a0fdd6dd04
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-01 11:25:50 +0530

    Swamy/01sep work (#74)
    * Initial Version
    
    * xUnit Test Project
    
    * Algo
    
    * Update MinHeap.cs
    
    * Unit Tests

---
+++ src/o1tkeklargestnumber/CS/TKEKLargestNumber.Algo.Tests/KthLargestTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using KthLargestElement;
+
+namespace KthLargestElementTests
+{
+    public class KthLargestTests
+    {
+        [Fact]
+        public void KthLargest_AddsCorrectly()
+        {
+            var kLargest = new KthLargest(3, new int[] { 3, 6, 9, 10 });
+            Assert.Equal(6, kLargest.Add(4));
+            Assert.Equal(7, kLargest.Add(7));
+            Assert.Equal(9, kLargest.Add(10));
+            Assert.Equal(9, kLargest.Add(8));
+            Assert.Equal(10, kLargest.Add(15));
+        }
+
+        [Fact]
+        public void KthLargest_HandlesEmptyInitialArray()
+        {
+            var kLargest = new KthLargest(1, new int[] { });
+            Assert.Equal(4, kLargest.Add(4));
+            Assert.Equal(5, kLargest.Add(5));
+        }
+    }
+}
---
+++ src/o1tkeklargestnumber/CS/TKEKLargestNumber.Algo/MinHeap.cs
@@ -0,0 +1,32 @@
+using System.Collections.Generic;
+
+namespace KthLargestElement
+{
+    public class MinHeap
+    {
+        private readonly PriorityQueue<int, int> _queue = new();
+        private readonly int _capacity;
+
+        public MinHeap(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public int Count => _queue.Count;
+
+        public void Add(int value)
+        {
+            _queue.Enqueue(value, value);
+
+            if (_queue.Count > _capacity)
+            {
+                _queue.Dequeue();
+            }
+        }
+
+        public int Peek()
+        {
+            return _queue.Peek();
+        }
+    }
+}
