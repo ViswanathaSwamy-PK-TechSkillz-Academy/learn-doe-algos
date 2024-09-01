d3ec614fc332f883f39f5ea8e12eded49e61d82c
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-09-01 11:15:15 +0530

    xUnit Test Project

---
+++ src/o1tkeklargestnumber/CS/TKEKLargestNumber.Algo.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace TKEKLargestNumber.Algo.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/o1tkeklargestnumber/CS/howtoexecute.md
@@ -0,0 +1,7 @@
+# How to Execute
+
+## Setup
+
+```powershell
+dotnet new console -o TKEKLargestNumber.Algo -f net8.0
+```
